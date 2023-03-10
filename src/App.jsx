fbd8aa95e4317bed3dc8eb47cccdc89f4992d5dd
enclar <107608010+enclar@users.noreply.github.com>
2023-03-10 20:54:49 +0800

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
                 }
             });
 
-            console.log("response: " + response);
+            console.log("response: " + response.body);
             const data = await response.json();
 
             if (response.ok) {
