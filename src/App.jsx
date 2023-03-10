04398744ecee138ac6ed152def87246261bec0e3
enclar <107608010+enclar@users.noreply.github.com>
2023-03-10 13:57:29 +0800

    build: set up proxy to backend and tested successfully, page is able to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+    // setting up state
+    const [tasks, setTasks] = useState([]);
+
+    // function to fetch the data and assign to state
+    const getTasks = async () => {
+        const response = await fetch("/api/task/list/");
+        const data = await response.json()
+        console.log(data);
+    };
+
+    // use effect
+    useEffect(() => {
+        getTasks();
+    });
+
     return (
         <div id="app">
             <p>This is my App</p>
