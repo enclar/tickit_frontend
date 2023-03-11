54a471e35481722335f7e72d7c2be434b8ddc802
enclar <107608010+enclar@users.noreply.github.com>
2023-03-11 13:39:35 +0800

    changed proxy port to localhost:10000

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: 'https://tickit-backend.onrender.com',
-        target: 'http://localhost:8000',
+        target: 'http://localhost:10000',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       }
