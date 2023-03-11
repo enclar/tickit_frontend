0a0bbeaf28a6393ec69020e575237bf11f07ee86
enclar <107608010+enclar@users.noreply.github.com>
2023-03-11 13:44:10 +0800

    build: changed proxy port to http://0.0.0.0:10000

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        // target: 'https://tickit-backend.onrender.com',
-        target: 'http://localhost:10000',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      }
+      "/api": "http://0.0.0.0:10000",
     },
   },
   plugins: [react()]
